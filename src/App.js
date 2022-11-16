54b8e40044523ccaf58055f64bfce06ef97d2cd9
pkkdpr <oliwia.giercza@gmail.com>
2022-11-16 10:09:30 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
-
+import foods from './foods.json';
+import React, { useState } from 'react';
+import { Card, Row, Col, Divider, Input, Button } from 'antd';
 function App() {
+  const [food, setFood] = useState(foods);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {food.map((oneFood) => {
+        return <> 
+          <p>{oneFood.name}</p>
+          <img src={oneFood.image} alt="food" style={{width:'100px', height:'auto' }}/>
+        </>;
+      })}
+
+      <Button> Hide Form / Add New Food </Button>
+      <div></div>
+      {/* Display Search component here */}
+
+      <Divider>Food List</Divider>
+      <p> FOOD_NAME_HERE </p>
+
+      <img src="FOOD_IMAGE_HERE" width={0} />
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {/* Render the list of Food Box components here */}
+      </Row>
     </div>
   );
 }
---
+++ src/components/AddFoodForm.js
@@ -0,0 +1,1 @@
+import { Card, Row, Col, Divider, Input, Button } from 'antd';
